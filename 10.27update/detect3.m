99583ebe3e5dffddf1aa2884a3aed1edaccb9a5a
Aaron T. Becker <aabecker@gmail.com>
2015-10-30 11:12:35 -0500

    Cleaned up warnings

--- 10.27update/detect3.m
+++ 10.27update/detect3.m
@@ -1,5 +1,6 @@
 %detect
 clear,clc
+format compact
 % ---------------------------------------
 % Kalman filter initialization
 R=[[0.2845,0.0045]',[0.0045,0.0455]'];
@@ -8,15 +9,17 @@ Q  = 0.01*eye(4);
 P  = 100*eye(4);
 dt = 1;
 A  = [[1,0,0,0]',[0,1,0,0]',[dt,0,1,0]',[0,dt,0,1]'];
-g  = 6; % pixels^2/time step
+g  = 1; % pixels^2/time step
 Bu = [0,0,0,g]';
 kfinit = 0;
 x      = zeros(100,4);
 % ---------------------------------------
-obj  = setupSystemObjects('First10Min.mp4'); % 创建视频对象
+obj  = setupSystemObjects('../First10Min.mp4'); % 创建视频对象
 num  = 0;
 num2 = 0;
 figure(1);
+cc = zeros(4000);
+cr = zeros(4000);
 while ~isDone(obj.reader)
     frame = readFrame(obj);
     [MR, MC] = size(frame);
@@ -31,14 +34,11 @@ while ~isDone(obj.reader)
         num2 = num2 + 1;
         [centroids, bboxes, mask1] = detectObjects(frame, obj, xy);
         imshow(frame)
+        title(['Frame ',num2str(num2), ' of' ])
         %extract ball
         if num2 == 1
-            cc_tmp = centroids(41,1);
-            cr_tmp = centroids(41,2);
-           
-%             cc_tmp = centroids(1,1);
-%             cr_tmp = centroids(1,2);
-           
+            cc_tmp = centroids(151,1);
+            cr_tmp = centroids(151,2);
         else
             cc_tmp = x(num2 - 1, 1);
             cr_tmp = x(num2 - 1, 2);
@@ -48,20 +48,20 @@ while ~isDone(obj.reader)
         for c = -0.9*radius: radius/20 : 0.9*radius
             r = sqrt(radius^2-c^2);
             %——————————————
-%             scrsz = get(0,'ScreenSize');
-%         for i=1:length (cc)
-%             fig=figure;
+            %             scrsz = get(0,'ScreenSize');
+            %         for i=1:length (cc)
+            %             fig=figure;
             %——————————————
             plot(cc(num2) + c, cr(num2) + r, 'g.');
             plot(cc(num2) + c, cr(num2) - r, 'g.');
             %--------------------------
-%             set(fig,'Position',scrsz);
-%             M(i)=getframe(fig);
-%             close;
-%         end
-%             movie2avi(M,'out.avi', 'compression', 'none');
+            %             set(fig,'Position',scrsz);
+            %             M(i)=getframe(fig);
+            %             close;
+            %         end
+            %             movie2avi(M,'out.avi', 'compression', 'none');
             %--------------------------
-
+            
         end
         %Slow motion!
         if kfinit==0
@@ -71,30 +71,30 @@ while ~isDone(obj.reader)
         end
         kfinit = 1;
         PP = A*P*A' + Q;  %predict next covariance
-        K  = PP*H'*inv(H*PP*H'+R); % Kalman Gain
+        K  = (PP*H')/(H*PP*H'+R); % Kalman Gain
         x(num2,:) = (xp + K*([cc(num2), cr(num2)]' - H*xp))';
         x(num2,:)
-        [cc(num2), cr(num2)];
+        display([cc(num2), cr(num2)]);
         P = (eye(4)-K*H)*PP;
-        hold on 
+        hold on
         
-           for c = -1*radius: radius/20 : 1*radius
+        for c = -1*radius: radius/20 : 1*radius
             r = sqrt(radius^2-c^2);
-%         %-------------------------------------------
-%           scrsz = get(0,'ScreenSize');
-%         for i=1:length (cc)
-%             fig=figure;
-        %-------------------------------------------
-     
+            %         %-------------------------------------------
+            %           scrsz = get(0,'ScreenSize');
+            %         for i=1:length (cc)
+            %             fig=figure;
+            %-------------------------------------------
+            
             plot(x(num2,1) + c, x(num2,2) + r, 'r.')
             plot(x(num2,1) + c, x(num2,2) - r, 'r.')
-%         %-------------------------------------------
-%          set(fig,'Position',scrsz);
-%             M(i)=getframe(fig);
-%             close;
-%         end
-%             movie2avi(M,'out.avi', 'compression', 'none');
-%         %-------------------------------------------
+            %         %-------------------------------------------
+            %          set(fig,'Position',scrsz);
+            %             M(i)=getframe(fig);
+            %             close;
+            %         end
+            %             movie2avi(M,'out.avi', 'compression', 'none');
+            %         %-------------------------------------------
         end
         pause(0.02)
     end
@@ -122,7 +122,7 @@ for i = 1:length(cc)
     [AS] = imread('tmp.png');
     [imind_,cm_] = rgb2ind(AS,256);
     if i == 1
-         imwrite(imind_,cm_, 'track.gif', 'gif', 'DelayTime',0.3);
+        imwrite(imind_,cm_, 'track.gif', 'gif', 'DelayTime',0.3);
     else
         imwrite(imind_,cm_, 'track.gif', 'gif', 'WriteMode','append', 'DelayTime',0.3);
     end
@@ -136,13 +136,13 @@ legend({'red is x position','green is y position'});
 % ------------------------------------------------
 %画速度变化图
 figure(4);
-ccV = [];
+ccV = zeros(length(cc)-1,1);
 for i = 1:length(cc)-1
-    ccV = [ccV, sqrt((cc(i) - cc(i+1)).^2 + (cr(i) - cr(i+1)).^2)];
+    ccV(i)= sqrt((cc(i) - cc(i+1)).^2 + (cr(i) - cr(i+1)).^2);
 end
 plot(ccV, 'b-*');
 ylabel('Speed');
-% 
+%
 %  %actual position of the umbrella
 %     plot(Q_loc(T));
 %     ylim=get(gca,'ylim');
